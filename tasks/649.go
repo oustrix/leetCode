fef5b9cd6a908442144869ba156c5c044b1b7f6a
oustrix <50222966+oustrix@users.noreply.github.com>
2023-05-04 18:30:14 +0300

    solved 649 problem but it so sloooooow

---
+++ tasks/649.go
@@ -0,0 +1,38 @@
+package tasks
+
+import (
+	"log"
+	"strings"
+)
+
+func Main() {
+	log.Println(predictPartyVictory("RDD"))
+}
+
+func predictPartyVictory(senate string) string {
+	for strings.Contains(senate, "R") && strings.Contains(senate, "D") {
+		for i := 0; i < len(senate); i++ {
+			if []rune(senate)[i] == []rune("D")[0] {
+				if strings.Contains(senate[i:], "R") {
+					senate = senate[:i] + strings.Replace(senate[i:], "R", "", 1)
+				} else {
+					senate = strings.Replace(senate, "R", "", 1)
+				}
+			} else {
+				if strings.Contains(senate[i:], "D") {
+					senate = senate[:i] + strings.Replace(senate[i:], "D", "", 1)
+				} else {
+					senate = strings.Replace(senate, "D", "", 1)
+				}
+			}
+
+			log.Printf("i: %d, senate: %s\n", i, senate)
+		}
+	}
+
+	if strings.Contains(senate, "R") {
+		return "Radiant"
+	} else {
+		return "Dire"
+	}
+}
