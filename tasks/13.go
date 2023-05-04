f2a6c32f0ea47af28b2dcfc5cbcc4b906b0fdf68
oustrix <50222966+oustrix@users.noreply.github.com>
2023-05-05 01:54:56 +0300

    solver 13. Roman to Iteger Not the most efficient solution

---
+++ tasks/13.go
@@ -0,0 +1,81 @@
+package tasks
+
+import "log"
+
+// 13. Roman to Integer
+
+func Main() {
+	log.Println(romanToInt("MCMXCIV"))
+}
+
+func romanToInt(s string) int {
+	nums := map[rune]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	num := 0
+
+	isPreviousI := false
+	isPreviousX := false
+	isPreviousC := false
+	for _, v := range s {
+		num += nums[v]
+		switch v {
+		case 'I':
+			isPreviousI = true
+			isPreviousX = false
+			isPreviousC = false
+
+		case 'V':
+			if isPreviousI {
+				num -= 2 * nums['I']
+				isPreviousI = false
+			}
+			isPreviousX = false
+			isPreviousC = false
+
+		case 'X':
+			if isPreviousI {
+				num -= 2 * nums['I']
+				isPreviousI = false
+			}
+			isPreviousX = true
+			isPreviousC = false
+
+		case 'L':
+			if isPreviousX {
+				num -= 2 * nums['X']
+				isPreviousX = false
+			}
+			isPreviousI = false
+			isPreviousC = false
+
+		case 'C':
+			if isPreviousX {
+				num -= 2 * nums['X']
+				isPreviousX = false
+			}
+			isPreviousC = true
+			isPreviousI = false
+
+		case 'D':
+			fallthrough
+		case 'M':
+			if isPreviousC {
+				num -= 2 * nums['C']
+				isPreviousC = false
+			}
+			isPreviousX = false
+			isPreviousI = false
+		}
+
+	}
+
+	return num
+}
